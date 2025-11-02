d37de1dea187eba41410fdde2225e1d621e5863c
Sai-Kolla85 <2300031410@kluniversity.in>
2025-11-02 23:47:27 +0530

    Change API base URL in productService.js
    Updated the API base URL for product service.

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://52.91.205.104:30083/back1/api/products";  // Updated Backend API URL
+const API_BASE_URL = "http://98.91.69.99:30083/back1/api/products";  // Updated Backend API URL
 
 // Fetch products by category (computers, mobiles, laptops, pendrives, or all)
 export const getProducts = async (category = "") => {
